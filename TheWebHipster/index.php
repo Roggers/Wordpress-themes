eb18cf38eb415f3dfafc41850b181ee4eb4a4628
Roggers <alexluisroggers@gmail.com>
2012-09-08 17:32:42 +0100

    second commit

---
+++ TheWebHipster/index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+get_header(); ?>
+
+ 
+
+  
+<?php
+      
+       get_template_part( 'loop', 'index' );
+      ?>
+
+  <div id="boxWrap">
+ <a href="http://www.twitter.com/Alexrodriguestm"> 
+ <section id="boxtweet" class="grid_4 red box">
+
+  </section> </a>
+  <section id="boxsponsor" class="grid_4 green box">
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6012648254620693";
+/* Alexrodrigues */
+google_ad_slot = "8230393311";
+google_ad_width = 300;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+   
+  </section>
+  <section id="boxrecentpost" class="grid_4 blue box">
+
+  </section>
+  </div>
+
+  </div>
+
+
+
+
+  <?php get_footer(); ?>
+
+
+   
+
+ 
